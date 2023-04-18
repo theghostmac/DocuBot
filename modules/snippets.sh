515092b49b4672e9595d2aa10bc325e5cb683558
MacBobby Chibuzor <theghostmac@gmail.com>
2023-04-18 13:14:33 +0100

    modified the modules scripts to access rules.config properly

--- modules/snippets.sh
+++ modules/snippets.sh
@@ -3,9 +3,9 @@
 # This script validates code snippets in documentation pages
 
 # Read configuration from rules.conf
-validate_snippets_enabled=$(grep -i 'validate_snippets_enabled' config/rules.conf | awk -F'=' '{print $2}')
-validate_snippets_languages=$(grep -i 'validate_snippets_languages' config/rules.conf | awk -F'=' '{print $2}')
-validate_snippets_exclude_paths=$(grep -i 'validate_snippets_exclude_paths' config/rules.conf | awk -F'=' '{print $2}')
+validate_snippets_enabled=$(grep -i 'validate_snippets_enabled' ../config/rules.conf | awk -F'=' '{print $2}')
+validate_snippets_languages=$(grep -i 'validate_snippets_languages' ../config/rules.conf | awk -F'=' '{print $2}')
+validate_snippets_exclude_paths=$(grep -i 'validate_snippets_exclude_paths' ../config/rules.conf | awk -F'=' '{print $2}')
 
 # Perform code snippet validation only if enabled
 if [[ $validate_snippets_enabled == "true" ]]; then
