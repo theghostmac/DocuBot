f9441ceb826c1456b46b4283c22fd62490a960c7
MacBobby Chibuzor <theghostmac@gmail.com>
2023-04-18 15:43:51 +0100

    attempt to run docubot | 1st

--- modules/spellcheck.sh
+++ modules/spellcheck.sh
@@ -3,9 +3,9 @@
 # This script performs spell checking on documentation pages
 
 # Read configuration from rules.conf
-spell_check_enabled=$(grep -i 'spell_check_enabled' ../config/rules.conf | awk -F'=' '{print $2}')
-spell_check_language=$(grep -i 'spell_check_language' ../config/rules.conf | awk -F'=' '{print $2}')
-spell_check_exclude_paths=$(grep -i 'spell_check_exclude_paths' ../config/rules.conf | awk -F'=' '{print $2}')
+spell_check_enabled=$(grep -i 'spell_check_enabled' config/rules.conf | awk -F'=' '{print $2}')
+spell_check_language=$(grep -i 'spell_check_language' config/rules.conf | awk -F'=' '{print $2}')
+spell_check_exclude_paths=$(grep -i 'spell_check_exclude_paths' config/rules.conf | awk -F'=' '{print $2}')
 
 # Perform spell checking only if enabled
 if [[ $spell_check_enabled == "true" ]]; then
